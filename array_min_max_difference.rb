172bfe3c96814caf89b7d5366d7dfbb16077d249
sun-diane Sun <dianesun1@gmail.com>
2023-04-01 04:25:49 +0000

    First commit

--- array_min_max_difference.rb
+++ array_min_max_difference.rb
@@ -1,7 +1,7 @@
 # Using the provided Array of numbers, output the number with the lowest value in
 # the array, the number with the highest value in the array, and the difference
 # between the highest value and the lowest value.
-# 
+#
 #   lowest_number
 #   highest_number
 #   difference
@@ -9,3 +9,6 @@
 # NOTE: for this exercise use the provided array (don't ask the user for a value, in this task).
 
 array = [12, 23, 41, 73, 19, 6]
+p array.min
+p array.max
+p array.max - array.min
